a6a2a0be275796952fcd6df2d46de8364defb37b
Alrons <titun766@gmail.com>
2024-07-19 22:00:56 +0500

    Start
    Added service and some models for him

---
+++ Assets/scripts/Models/addedItemModel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddedItemModel
+{
+    public int id;
+    public int userId;
+    public string title;
+    public string description;
+    public int price; // Change the type of price to int
+    public int ñurrency;
+    public string image;
+    public int place;
+    public int health;
+    public int power;
+    public int xPower;
+
+    public AddedItemModel(int id, int userId, string title, string description, int price, int currency, string image, int place, int health, int power, int xPower)
+    {
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.ñurrency = currency;
+        this.image = image;
+        this.place = place;
+        this.health = health;
+        this.power = power;
+        this.xPower = xPower;
+    }
+}
---
+++ Assets/scripts/Models/itemServiceProperties.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using UnityEngine;
+
+public class itemServiceProperties
+{
+    // Use a constant for the base URL
+    public readonly string BaseUrl = "https://localhost:7139/api/";
+
+    // Use a readonly field for the HTTP client
+    public HttpClient _httpClient { get; set; }
+
+    // Use a readonly field for the URL
+    public string _AddedItemsUrl { get; set; }
+
+    public string _Items { get; set; }
+
+    public string _SizeTables { get; set; }
+
+    public string _OurTables { get; set; }
+
+    public itemServiceProperties()
+    {
+        // Initialize the HTTP client
+        _httpClient = new HttpClient { BaseAddress = new Uri(BaseUrl) };
+
+        // Initialize the URL
+        _AddedItemsUrl = "AddedItems/";
+        _Items = "Items/";
+        _SizeTables = "SizeTables/";
+        _OurTables = "OurTables/";
+    }
+}
